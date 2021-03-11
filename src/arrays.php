68a855da8c76a402c0ecb6ad1faf253c4a90e8f7
EMMANUEL HAVET <ehavet@ladrome.fr>
2021-03-11 11:43:17 +0100

    Ajout outils arrays
    Ajout de fonction de tri sur un tableau bi-dimensionnel

---
+++ src/arrays.php
@@ -0,0 +1,53 @@
+<?php
+
+/*********
+*
+*  File: arrays.php
+*  Date: 01/03/2021
+*  Suject : advanced fucntion to work on arrays
+*
+**********/
+
+namespace ladromelaboratoire\tools;
+use DateTime;
+
+class arrays {
+	
+	public static function array2DSort($records, $field, $reverse=false) {
+		$hash = array();
+	   
+		foreach($records as $record) {
+			$hash[$record[$field]] = $record;
+		}
+	   
+		($reverse)? krsort($hash) : ksort($hash);
+	   
+		$records = array();
+	   
+		foreach($hash as $record) {
+			$records []= $record;
+		}
+	   
+		return $records;
+	}
+	
+	public static function array2DSortDate($records, $field, $format_in = 'd/m/Y', $format_out = 'Ymd', $reverse=false) {
+		$hash = array();
+	   
+		foreach($records as $record) {			
+			$hash[DateTime::createFromFormat($format_in, $record[$field])->format($format_out)] = $record;
+		}
+	   
+		($reverse)? krsort($hash) : ksort($hash);
+	   
+		$records = array();
+	   
+		foreach($hash as $record) {
+			$records []= $record;
+		}
+	   
+		return $records;
+	}
+}
+
+?>
