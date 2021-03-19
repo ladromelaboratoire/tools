9688194d722bbce1f753178a272e5214b2d30a5a
EMMANUEL HAVET <ehavet@ladrome.fr>
2021-03-19 13:57:32 +0100

    Amélioration exple htmlarrays

--- tests/arrayhtml.php
+++ tests/arrayhtml.php
@@ -4,9 +4,15 @@ use ladromelaboratoire\tools\htmltools;
 
 $data = [['Col1' => '1.1', 'Col2' => '1.2'],
 		['Col1' => '2.1', 'Col2' => '2.2']];
-		
+	
+echo "<p>";	
 //This is an assoc array
 echo htmltools::array2html($data, true);
+
+echo "</p><p>";
+//This is not an assoc array
+echo htmltools::array2html($data, false);
+echo "</p>";
 
 
 ?>
